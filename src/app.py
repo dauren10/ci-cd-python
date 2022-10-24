b676a60388aeb2a44eda9720b73711a809a2bd8e
Dauren Shalabayev <d.shalabayev@bsl.dev>
2022-10-24 23:06:53 +0600

    fix route

--- src/app.py
+++ src/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/")
-def index():
+@app.route("/")
+def hello_world():
     return 'Hello'
 
 if __name__ == "__main__":
--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -1,4 +1,4 @@
-from app import index
+from app import hello_world
 
 def test_index():
-    assert index() == 'Hello'
+    assert hello_world() == 'Hello'
